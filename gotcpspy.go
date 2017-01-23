552c4303e4a65dd4715e0953d73dd0155db1521d
example <example@local.tld>
2017-01-23 15:12:22 +0600

    should omit type *string from declaration of var host; it will be inferred from the right-hand side

--- gotcpspy.go
+++ gotcpspy.go
@@ -15,9 +15,9 @@ import (
 )
 
 var (
-	host        *string = flag.String("host", "", "target host or address")
-	port        *string = flag.String("port", "0", "target port")
-	listen_port *string = flag.String("listen_port", "0", "listen port")
+	host        = flag.String("host", "", "target host or address")
+	port        = flag.String("port", "0", "target port")
+	listen_port = flag.String("listen_port", "0", "listen port")
 )
 
 func die(format string, v ...interface{}) {
